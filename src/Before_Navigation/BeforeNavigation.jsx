3eb2cd992f126c0ab7fad34dd8246dcc24a79731
Yugyeong Park <bonbonly7@naver.com>
2024-06-09 03:37:44 +0900

    로그인 전 네비게이션은 검색과 로그인 버튼 존재

---
+++ src/Before_Navigation/BeforeNavigation.jsx
@@ -0,0 +1,34 @@
+import FindingNemo from "./components/FindingNemo";
+import Make from "./components/Make";
+import Shop from "./components/Shop";
+import SignIn from "./components/SignIn";
+import "./BeforeNavigation.css";
+
+const Navigation = () => {
+  return (
+    <div className="navigation">
+      <div className="text">
+
+        <div className="finding-nemo">
+          <img className="clownfish" alt="Clownfish" src="img/clownfish.png" />
+          <FindingNemo className="finding-nemo-text" />
+        </div>
+
+        <div className="menu">
+          <Shop className="SHOP-instance" />
+          <Make className="MAKE-instance" />
+        </div>
+
+        <img className="Magnifier" alt="Magnifier" src="img/magnifier.png" />
+
+        <div className="SIGN-IN">
+          <div className="rectangle" />
+          <SignIn className="SIGN-IN-instance" />
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;
