177fb62a841548f796e52929d49061dd74ae5946
Yugyeong Park <bonbonly7@naver.com>
2024-06-16 19:30:30 +0900

    의미없는 div 삭제

--- src/SignIn/components/DonTHaveAn.jsx
+++ src/SignIn/components/DonTHaveAn.jsx
@@ -3,12 +3,8 @@ import './DonTHaveAn.css';
 const DonTHaveAn = () => {
   return (
     <div className="dontHaveAnAccountSignUp">
-      <div className="dontHaveAnContainer">
-        <span className="dontHaveAnContainer1">
           <span className="dontHaveAn">{`Don’t have an account?  `}</span>
           <span className="signUp">Sign up</span>
-        </span>
-      </div>
     </div>);
 };
 
