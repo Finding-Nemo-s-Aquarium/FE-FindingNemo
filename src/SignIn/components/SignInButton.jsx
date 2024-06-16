8261c4d67bfdd3931c557b6a23bcf1bc612c44ae
Yugyeong Park <bonbonly7@naver.com>
2024-06-16 19:31:44 +0900

    SignInButton에 기능 추가

--- src/SignIn/components/SignInButton.jsx
+++ src/SignIn/components/SignInButton.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
+import "./SignInButton.css";
 
-function SignInButton() {
-  return <div className="signin-button">Sign in</div>;
+const SignInButton = ({ className, setIsLoggedIn }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    setIsLoggedIn(true);
+    navigate('/home');
+  };
+
+  return (
+    <div className={`signin-button ${className}`} onClick={handleClick}>
+      <span className="text-wrapper">Sign in</span>
+    </div>
+  );
 }
+
+SignInButton.propTypes = {
+  className: PropTypes.string,
+  setIsLoggedIn: PropTypes.func.isRequired,
+};
 
 export default SignInButton;
