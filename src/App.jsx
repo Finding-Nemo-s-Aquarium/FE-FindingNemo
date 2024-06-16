b8d20e6f0b8736b7936be420f42d9d9c34dce0db
Yugyeong Park <bonbonly7@naver.com>
2024-06-16 19:16:27 +0900

    로그인 유무에 따라 페이지 접근이 달라지도록 App 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 import SignIn from './SignIn/SignIn.jsx';
 import Home from './Home/Home.jsx';
 import Shop from './Shop/Shop.jsx';
@@ -6,14 +7,16 @@ import Make from './Make/Make.jsx';
 import './App.css';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
       <div className="App">
         <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/shop" element={<Shop />} />
+          <Route path="/home" element={<Home isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} />
+          <Route path="/signin" element={<SignIn setIsLoggedIn={setIsLoggedIn} />} />
+          <Route path="/shop" element={<Shop isLoggedIn={isLoggedIn} />} />
           <Route path="/make" element={<Make />} />
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} />
         </Routes>
       </div>
   );
