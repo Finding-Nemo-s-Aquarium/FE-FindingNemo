bf77028e022e53e1531d9779e81ec10715fbf90c
Yugyeong Park <bonbonly7@naver.com>
2024-06-16 19:24:30 +0900

    category별로 상품을 선택할 수 있도록 Shop 로직 변경

--- src/Shop/Shop.jsx
+++ src/Shop/Shop.jsx
@@ -1,47 +1,84 @@
-import Navigation from '../After_Navigation/AfterNavigation';
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import BeforeNavigation from "../Navigation/BeforeNavigation";
+import AfterNavigation from '../Navigation/AfterNavigation';
 import "./Shop.css";
 
-const Shop = () => {
+const Shop = ({ isLoggedIn }) => {
+    const [selectedCategory, setSelectedCategory] = useState(null);
+
+    const categories = {
+        Aquarium: [
+            { name: "Fish tank", img: "img2/fishbowl1.png" }, // fish_tank
+            // { name: "Fish bowl", img: "img2/fish_bowl.png" } // fish_bowl
+        ],
+        Fish: [
+            // { name: "Guppy", img: "img2/guppy.png" }, // guppy
+            { name: "Goldfish", img: "img2/goldfish.png" }, // goldfish
+            { name: "Betta", img: "img2/beta.png" }, // betta
+            { name: "Angelfish", img: "img2/angelfish.png" }, // angelfish
+            { name: "Tetra", img: "img2/tetra.png" } // tetra
+        ],
+        Plant: [
+            // { name: "Blyxa japonica", img: "img2/blyxa_japonica.png" }, // blyxa_japonica
+            { name: "American water sprite", img: "img2/america.png" }, // american_water_sprite
+            { name: "Vallisneria", img: "img2/balis.png" } // vallisneria
+        ],
+        Stone: [
+            { name: "Egg stone", img: "img2/eggstone.png" }, // egg_stone
+            { name: "Blue dragon stone", img: "img2/bluedragonstone.png" }, // blue_dragon_stone
+            { name: "Volcanic stone", img: "img2/volcanostone.png" } // volcanic_stone
+        ],
+        Flooring: [
+            { name: "Black sand", img: "img2/blackfloor.png" }, // black_sand
+            { name: "White sand", img: "img2/whitefloor.png" }, // white_sand
+            { name: "Multicolored sand", img: "img2/fivecolorfloor.png" } // multicolored_sand
+        ]
+    };
+
+    const handleCategoryClick = (category) => {
+        setSelectedCategory(category);
+    };
+
     return (
         <div className="Background">
-            <Navigation />
+            <div className="navigation">
+                {isLoggedIn ? <AfterNavigation /> : <BeforeNavigation />}
+            </div>
 
-            <div className="hero">
-                <img src="img/main_nemo.png" alt="Clownfish" className="hero-image" />
+            <div className="top">
+                <img src="img/main_nemo.png" alt="Clownfish" className="top-image" />
             </div>
 
             <div className="category-bar">
-                <div className="category">
-                    <div className="category-title">Fish</div>
-                    <div className="category-items">Guppy</div>
-                </div>
-                <div className="category">
-                    <div className="category-title">Fishbowl</div>
-                    <div className="category-items">Goldfish</div>
-                </div>
-                <div className="category">
-                    <div className="category-title">Plant</div>
-                    <div className="category-items">Betta</div>
-                </div>
-                <div className="category">
-                    <div className="category-title">Stone</div>
-                    <div className="category-items">Angelfish</div>
-                </div>
-                <div className="category">
-                    <div className="category-title">Flooring</div>
-                    <div className="category-items">Tetra</div>
-                </div>
+                {Object.keys(categories).map(category => (
+                    <div className="category" key={category}>
+                        <div className="category-title" onClick={() => handleCategoryClick(category)}>
+                            {category}
+                        </div>
+                        {selectedCategory === category && (
+                            <div className="category-items">
+                                {categories[category].map(item => (
+                                    <div className="category-item" key={item.name}>{item.name}</div>
+                                ))}
+                            </div>
+                        )}
+                    </div>
+                ))}
             </div>
             <div className="gallery">
-                <div className="gallery-item"><img src="img/fish.png" alt="Guppy" /></div>
-                <div className="gallery-item"></div>
-                <div className="gallery-item"></div>
-                <div className="gallery-item"></div>
-                <div className="gallery-item"></div>
-                <div className="gallery-item"></div>
+                {selectedCategory && categories[selectedCategory].map(item => (
+                    <div className="gallery-item" key={item.name}>
+                        <img src={item.img} alt={item.name} />
+                    </div>
+                ))}
             </div>
         </div>
     );
+};
+
+Shop.propTypes = {
+    isLoggedIn: PropTypes.bool.isRequired,
 };
 
 export default Shop;
