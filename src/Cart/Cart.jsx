8dca1b74ec83562af6e5cecef0b056bc99f7f536
eunhye <gp79810@gmail.com>
2024-06-17 20:22:52 +0900

    Cart : /data.json받아와 파싱 및 렌더링 설정

--- src/Cart/Cart.jsx
+++ src/Cart/Cart.jsx
@@ -1,44 +1,50 @@
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import BeforeNavigation from "../Navigation/BeforeNavigation";
 import AfterNavigation from "../Navigation/AfterNavigation"; 
 import './Cart.css';
 
-const dummyData = [
-  { id: 1, name: '상품 1', price: 10000, amount: 1, src: "/img2/Guppy.png" },
-  { id: 2, name: '상품 2', price: 20000, amount: 2, src: "/img2/Guppy.png" },
-  { id: 3, name: '상품 3', price: 30000, amount: 1, src: "/img2/Guppy.png" },
-  { id: 1, name: '상품 1', price: 10000, amount: 1, src: "/img2/Guppy.png" },
-  { id: 2, name: '상품 2', price: 20000, amount: 2, src: "/img2/Guppy.png" },
-  { id: 3, name: '상품 3', price: 30000, amount: 1, src: "/img2/Guppy.png" },
-  { id: 1, name: '상품 1', price: 10000, amount: 1, src: "/img2/Guppy.png" },
-  { id: 2, name: '상품 2', price: 20000, amount: 2, src: "/img2/Guppy.png" },
-  { id: 3, name: '상품 3', price: 30000, amount: 1, src: "/img2/Guppy.png" }
-];
-
 function Cart({ isLoggedIn }) {
-  
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch('/data.json')
+      .then(response => response.json())
+      .then(data => setItems(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
   return (
     <div className="Cart">
-      <img id="cart-background" src="img2/cart-background.png" alt="배경화면" />
+      <img id="cart-background" src="public/img/cart-background.png" alt="배경화면" />
 
       <div className="cart-head">
         {isLoggedIn ? <AfterNavigation /> : <BeforeNavigation />}
       </div>
       <h1 id='title'>C A R T</h1>
+      <h2 id='itemlist'>ITEM LIST</h2>
+      
       <div className="cart-body">
-        <h2>ITEM LIST</h2>
-        {dummyData.map(item => (
-          <div key={item.id} className='cart-item'>
-            <img className='cart-item-img' src={item.src} alt="사진" />
-              <span className='cart-item-name'>{item.name}</span>
-              <span className='cart-item-price'>{item.price.toLocaleString()}$</span>
-              <span className='cart-item-amount'>Amount: {item.amount}</span>
-          </div>
-        ))}
-        <div className="total">
-          Total Price: {dummyData.reduce((acc, item) => acc + item.price * item.amount, 0).toLocaleString()}원
-        </div>
+        {items.length === 0 ? (
+          <p style={{textAlign:"center",fontSize:'25px',borderRadius:'20px',padding:'10px'}}>Your cart is empty.</p>
+        ) : (
+          <>
+            {items.map(item => (
+              <div key={item.id} className='cart-item'>
+                <img className='cart-item-img' src={item.src} alt="사진" />
+                <span className='cart-item-name'>{item.name}</span>
+                <span className='cart-item-price'>{item.price.toLocaleString()}$</span>
+                <span className='cart-item-amount'>Amount: {item.amount}</span>
+              </div>
+            ))}
+            
+          </>
+        )}
+       
       </div>
+      <div className="cart-total">
+              Total Price: {items.reduce((acc, item) => acc + item.price * item.amount, 0).toLocaleString()}$
+            </div>
     </div>
   );
 }
