8fbf982e13ed999a86b196e4c654ce067fdf0d11
최가은 <eunii@NUTELLA-MacBookPro.local>
2024-06-17 22:19:27 +0900

    장바구니 백엔드 연결 중 </div> 오류 해결

--- src/Cart/Cart.jsx
+++ src/Cart/Cart.jsx
@@ -30,32 +30,29 @@ function Cart({ isLoggedIn }) {
         {isLoggedIn ? <AfterNavigation /> : <BeforeNavigation />}
       </div>
       <div className='cart-body'>
-      <h1 id='title'>C A R T</h1>
-      <h2 id='itemlist'>ITEM LIST</h2>
-      <div className="cart-item-list">
-        {items.length === 0 ? (
-          <p style={{textAlign:"center",fontSize:'25px',borderRadius:'20px',padding:'10px',opacity:'0.5'}}>Your cart is empty.</p>
-        ) : (
-          <>
-            {items.map((item, index) => (
-              <div key={index} className='cart-item'>
-                <img className='cart-item-img' src={item.img_url} alt={item.name} />
-                <span className='cart-item-name'>{item.name}</span>
-                <span className='cart-item-price'>{item.price.toLocaleString()}$</span>
-                <span className='cart-item-amount'>Amount: {item.amount}</span>
-              </div>
-            ))}
-          </>
-        )}
+        <h1 id='title'>C A R T</h1>
+        <h2 id='itemlist'>ITEM LIST</h2>
+        <div className="cart-item-list">
+          {items.length === 0 ? (
+            <p style={{textAlign:"center",fontSize:'25px',borderRadius:'20px',padding:'10px',opacity:'0.5'}}>Your cart is empty.</p>
+          ) : (
+            <>
+              {items.map((item, index) => (
+                <div key={index} className='cart-item'>
+                  <img className='cart-item-img' src={item.img_url} alt={item.name} />
+                  <span className='cart-item-name'>{item.name}</span>
+                  <span className='cart-item-price'>{item.price.toLocaleString()}$</span>
+                  <span className='cart-item-amount'>Amount: {item.amount}</span>
+                </div>
+              ))}
+            </>
+          )}
+        </div>
+        <h2 id='itemlist'>TOTAL</h2>
+        <div className="cart-total">
+          Total Price: {total.toLocaleString()}$
+        </div>
       </div>
-      <h2 id='itemlist'>TOTAL</h2>
-      <div className="cart-total">
-        Total Price: {total.toLocaleString()}$
-      </div>
-              Total Price: {items.reduce((acc, item) => acc + item.price * item.amount, 0).toLocaleString()}$
-            </div>
-      </div>
-
     </div>
   );
 }
